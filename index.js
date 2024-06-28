1caf7ce7eded907e3819e688f316df5ce6edf146
MorenaDlamini <dlaminimorena@gmail.com>
2024-06-28 18:10:41 +0200

    button function

---
+++ index.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('editProfileButton');
+button.addEventListener('click', function() {
+    // Redirect to the desired URL
+    window.location.href = 'https://example.com/edit-profile';
+});
